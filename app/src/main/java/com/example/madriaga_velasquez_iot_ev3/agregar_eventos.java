fc73a232ef29483504c1fdc8cb0a9bc15a983745
luismadriaga <luis.madriaga03@inacapmail.cl>
2022-12-20 04:15:21 -0300

    Quinto commit

---
+++ app/src/main/java/com/example/madriaga_velasquez_iot_ev3/agregar_eventos.java
@@ -0,0 +1,14 @@
+package com.example.madriaga_velasquez_iot_ev3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class agregar_eventos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_eventos);
+    }
+}
