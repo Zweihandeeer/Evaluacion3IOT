2c3c2cdd2aee44604a82aaa32c5c40996d23241d
Luis Velasquez <lvelasquez17903@gmail.com>
2022-12-20 01:35:29 -0300

    Primer commit

---
+++ app/src/main/java/com/example/madriaga_velasquez_iot_ev3/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.madriaga_velasquez_iot_ev3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Sistema ingreso de eventos");
+    }
+}
